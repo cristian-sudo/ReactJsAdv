14792b57347c9d058904445d64a6aad32bc02198
cristian-sudo <cplop21@gmail.com>
2021-09-24 10:36:11 +0100

    UseEffect(Hooks)

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,27 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+
+  const [size,setsize] = useState(window.innerWidth)//set the size
+  
+  //a function that change the size to the current window width
+  const modify =()=>{
+  setsize(window.innerWidth)
+  }
+
+//creating an use effect that every time we render and change somenting on the page it creates a new event
+  useEffect(()=>{
+    window.addEventListener("resize", modify)
+    return ()=>{
+    window.removeEventListener("resize",modify)
+  }
+  })
+
+  console.log(size)
+  return <>
+  <h1>window size</h1>
+  <h2>{size}</h2>
+  </>;
 };
 
 export default UseEffectCleanup;
