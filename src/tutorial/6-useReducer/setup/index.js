13113a64e99e8ef79b0c691697856635b6644660
cristian-sudo <cplop21@gmail.com>
2021-10-05 13:01:39 +0100

    Use Reducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,9 +2,59 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
+//have the privious state and the action to do based on type
+const reducer=(state,action)=>{
+}
+
+const defaultState={
+people:[],
+isModalOpen:false,
+modalContent:""
+}
+
+
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name,setName]=useState("")
+  //state is the state shown 
+  //dispatch is the function
+  //reducer is the funtion that will do somenthing to our state
+  //defaultState is the default value
+  const [state,dispatch]=useReducer(reducer,defaultState)
+  const [key,setKey]=useState(0)
+
+  const SubmitHandle = (e)=>{
+  e.preventDefault()
+  if(name){
+dispatch({type: "Testing"})
+  }else{
+  }
+  
+  }
+
+  return <>
+
+  {state.isModalOpen && <Modal modalContent={state.modalContent}/>}
+  
+  <form className="form" onSubmit={SubmitHandle}>
+<input type="text" 
+name="Myinput"
+onChange={(e)=>{setName(e.target.value)}}
+/>
+<button type="submit">Add</button>
+  </form>
+  <div>
+    {state.people.map((a)=>{
+      return <p className="item" key={a.id}>
+        {a.name}
+      </p>
+    })}
+  </div>
+
+  </>;
 };
 
 export default Index;
+
+
+
